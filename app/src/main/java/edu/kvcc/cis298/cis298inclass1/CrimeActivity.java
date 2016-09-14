15c30c050e36218fc8e40ae26231e298f5a23f22
Jordan Koehler <kurai3@gmail.com>
2016-09-14 16:28:54 -0400

    Adds a question and two possible answers

--- app/src/main/java/edu/kvcc/cis298/cis298inclass1/CrimeActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298inclass1/CrimeActivity.java
@@ -2,12 +2,41 @@ package edu.kvcc.cis298.cis298inclass1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CrimeActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        mTrueButton= (Button) findViewById(R.id.true_button);
+        mFalseButton= (Button) findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CrimeActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(CrimeActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
